f5a15047cab01ee9064f0cd2603ddc1b8e5b0c0a
Mario <mga.375@gmail.com>
2022-01-29 11:28:53 -0600

    shiny app v1 al 100

--- App.R
+++ App.R
@@ -5,24 +5,59 @@ library(lubridate)
 library(dplyr)
 library(ggplot2)
 library(magrittr)
-source("~/Main.R")
+library(plotly)
+source("./Main.R")
+source("./.Rprofile")
+
+
+# rsconnect::setAccountInfo(name='mariogalaviz',
+#                           token=token.shiny,
+#                           secret=secret.shiny)
 
 ui <- fluidPage(
-  sliderInput(
-    inputId = 'num',
-    label = 'holi',
-    value = 27, min = 1, max = 50
-  )
+  theme = shinytheme('lumen'),
+  tags$h1('Métricas Lavandaal'),
+  tags$h2('Volumen de negocio'),
+  tags$h3('Tabla de órdenes por día'),
+  'Fuente: BD',
+  plotlyOutput('tablaOrdenes'),
+  tags$h3('Tabla de valor de órdenes por día'),
+  'Fuente: BD',
+  plotlyOutput('tablaIngresos'),
+  tags$h2('Engagement'),
+  tags$h3('Tabla de actualizaciones promedio por orden'),
+  'Fuente: BD',
+  plotlyOutput('tablaActualizaciones'),
+  tags$h3('Tabla de % ordenes con calificaciones'),
+  'Fuente: BD',
+  plotlyOutput('tablaCalificaciones'),
+  tags$h3('Tabla de duración de sesión'),
+  'Fuente: GA',
+  plotlyOutput('graficaDuracion'),
+  tags$h3('Visitas a diferentes páginas'),
+  'Fuente: GA',
+  plotlyOutput('graficaPageviews')
 )
 
 server <- function(input, output) {
-  theme = shinytheme('lumen')
-  titlePanel('Métricas Lavandaal')
-  sidebarLayout(
-    sidebarPanel(),
-    mainPanel(),
-  )
-  
+  output$tablaOrdenes <- renderPlotly({
+    tabla.ordenes
+  })
+  output$tablaActualizaciones <- renderPlotly({
+    tabla.actualizaciones
+  })
+  output$tablaCalificaciones <- renderPlotly({
+    tabla.calificaciones
+  })
+  output$tablaIngresos <- renderPlotly({
+    tabla.ingresos
+  })
+  output$graficaDuracion <- renderPlotly({
+    plot.duracionSesion
+  })
+  output$graficaPageviews <- renderPlotly({
+    plot.pageViews
+  })
 }
 
 shinyApp(server = server, ui = ui)
--- Main.R
+++ Main.R
@@ -4,7 +4,7 @@ library(dplyr)
 library(ggplot2)
 library(magrittr)
 library(RGoogleAnalytics)
-source("~/.Rprofile")
+source("./.Rprofile")
 
 con <- dbConnect(RPostgres::Postgres(),
                  dbname = 'dcf8hlqhi8reh0',
@@ -33,11 +33,26 @@ tablaPromedios <- tablaOrdenes %>% group_by(fecha_creacion_formato) %>%
             ordenes = n()
             )
 # Gráficas
-ggplot(data=tablaPromedios, aes(fecha_creacion_formato, ordenes, group=1)) +
-  geom_line()
+tabla.ordenes <- ggplot(data=tablaPromedios,
+                        aes(fecha_creacion_formato, ordenes, group=1)) +
+  geom_line() +
+  geom_point()
 
-ggplot(data=tablaPromedios, aes(fecha_creacion_formato, ingresos, group=1)) +
-  geom_line()
+tabla.ingresos <- ggplot(data=tablaPromedios,
+                         aes(fecha_creacion_formato, ingresos, group=1)) +
+  geom_line() +
+  geom_point()
+
+tabla.calificaciones <- ggplot(data=tablaPromedios,
+                               aes(fecha_creacion_formato, tiene_calificacion, group=1)) +
+  geom_line() +
+  geom_point()
+
+tabla.actualizaciones <- ggplot(data=tablaPromedios,
+                         aes(fecha_creacion_formato, actualizaciones_promedio, group=1)) +
+  geom_line() +
+  geom_point()
+
 
 ## Conección a GA
 
@@ -52,20 +67,41 @@ if(!file.exists('./token')) {
   load('./token')
 }
 
+ValidateToken(token)
 
-start.date <- '2022-01-28'
+start.date <- format(as.Date(Sys.Date()-6),  '%Y-%m-%d')
 
-end.date <- '2022-01-28'
+end.date <- format(as.Date(Sys.Date()),  '%Y-%m-%d')
 
 query.list <- Init(start.date = start.date,
                    end.date = end.date,
-                   dimensions = "ga:hour,ga:dayOfWeek",
-                   metrics = "ga:sessions",
+                   dimensions = "ga:day,ga:month",
+                   metrics = "ga:sessionDuration,ga:sessions", #la duración está en segundos
                    table.id = view.id)
 
 ga.query <- QueryBuilder(query.list)
 
 ga.data <- GetReportData(ga.query, token)
 
+ga.data$fecha <- paste(ga.data$month,'-',ga.data$day, sep = '')
 
+plot.duracionSesion <- ggplot(data=ga.data,
+                              aes(fecha, sessionDuration, group=1)) +
+  geom_line()
 
+query.list.pageViews <- Init(start.date = start.date,
+                   end.date = end.date,
+                   dimensions = "ga:day,ga:month,ga:pagePath",
+                   metrics = "ga:uniquePageviews",
+                   table.id = view.id)
+
+ga.query.pageViews <- QueryBuilder(query.list.pageViews)
+
+ga.data.pageViews <- GetReportData(ga.query.pageViews, token)
+
+ga.data.pageViews$fecha <- paste(ga.data.pageViews$month,'-',ga.data.pageViews$day, sep = '')
+
+plot.pageViews <- ggplot(data=ga.data.pageViews,
+          aes(fecha, uniquePageviews, colour=pagePath, group=1)) +
+  geom_line() +
+  geom_point()
